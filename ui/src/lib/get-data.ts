51015e844a1a1a69a197cb22e603e30aa808a7cc
3pwd <me@3pwd.xyz>
2023-05-23 23:02:37 +0200

    :wrench: Set time for next cache invalidation interval

--- ui/src/lib/get-data.ts
+++ ui/src/lib/get-data.ts
@@ -1,5 +1,5 @@
 export async function getData<T>(url: string): Promise<T> {
-  const res = await fetch(url)
+  const res = await fetch(url, { next: { revalidate: 12 } })
 
   if (!res.ok) throw new Error('Failed to fetch data')
 
